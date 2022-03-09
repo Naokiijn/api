d580f6a39354409ef8291456256b29f05ccbdeb1
Naoki <78860957+Naokiijn@users.noreply.github.com>
2022-03-08 21:32:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    fs.writeFileSync('./teste.json', JSON.stringify({ data: "KKKKKKKK" }, null, '\t'))
+    //fs.writeFileSync('./teste.json', JSON.stringify({ data: "KKKKKKKK" }, null, '\t'))
     res.json({ data: "Hello World!!!", errors: {} })
 })
 
